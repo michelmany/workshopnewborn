5bf346f09d71b138918c5cfe5ecb97dc74919ab5
Michel Moraes <michel.moraes@helloworldlondon.co.uk>
2018-03-13 00:52:41 +0100

    arquivos plugins minificados + index

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ $Session = new Session;
         ?>
 
 
-    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     
     <script src="<?= INCLUDE_PATH; ?>/plugins/allplugins.min.js"></script>
